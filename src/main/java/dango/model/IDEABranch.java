5b2677a879686e38af933296ad67ca73929b98b1
dango <dango_zz@163.com>
2018-08-24 11:34:31 +0800

    Merge remote-tracking branch 'remotes/origin/master' into idea2
    # Conflicts:
    #	src/main/java/dango/model/IDEABranch.java

--- src/main/java/dango/model/IDEABranch.java
+++ src/main/java/dango/model/IDEABranch.java
@@ -6,7 +6,7 @@ package dango.model;
  * @Description:
  */
 public class IDEABranch {
-    private String string="IDEA22222222222222222222";
+    private String string="IDEA22222222222222222222555556";
     private String string2="dango";
     private String string3="dango2";
 }
